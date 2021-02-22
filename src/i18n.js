94eb534b6099d78a57522740e1aa20ed38a5f9e7
dotX12 <hash@shitposting.team>
2021-02-23 01:42:05 +0300

    WTF imports

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import {initReactI18next} from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 
 import translationCN from './locales/cn/translation.json';
 import translationDE from './locales/de/translation.json';
